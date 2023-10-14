632cb11544702de7ede0fcfc67f8c4e017e54a5c
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-10-14 14:07:38 -0400

    Ui

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('test');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
