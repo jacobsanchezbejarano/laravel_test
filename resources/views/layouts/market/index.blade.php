6950766eb01e0aa070c734893f76ed571729297c
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-10-14 14:35:48 -0400

    Updating login

---
+++ resources/views/layouts/market/index.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <h1>Anuncios de Negocios</h1>
+
+    <!-- Ejemplo de un anuncio de negocio utilizando Bootstrap -->
+    <div class="card">
+        <img src="imagen-negocio.jpg" class="card-img-top" alt="Imagen del negocio">
+        <div class="card-body">
+            <h5 class="card-title">Nombre del Negocio</h5>
+            <p class="card-text">Descripción breve del negocio.</p>
+            <p class="card-text">Número de contacto: <a href="tel:123456789">123-456-789</a></p>
+        </div>
+    </div>
+
+    <!-- Puedes replicar este bloque de código para mostrar múltiples anuncios de negocios -->
+</div>
